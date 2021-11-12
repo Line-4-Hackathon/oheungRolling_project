b432307356f242a8acb3d8fa61d058f0de5acae9
YeonJin55 <gkduswls1025@naver.com>
2021-11-13 03:09:48 +0900

    update views

--- oheungRolling_project/urls.py
+++ oheungRolling_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rollingPaper import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name='main'),
+    path('detail/<str:id>', views.detail, name='detail'),
+    path("codeconfirm", views.codeconfirm, name='codeconfirm'),
 ]
--- rollingPaper/views.py
+++ rollingPaper/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth import authenticate
+from django.contrib import messages
+import uuid
+import base64
+from .models import RollingInfo
+from .models import Comment
+from django.utils import timezone
+from datetime import datetime, timedelta
+from django.utils.dateformat import DateFormat
+
 
 # Create your views here.
+
+
+#main 페이지를 띄워주는 함수
+def main(request):
+    return render(request, "main.html")
+
+
+#코드 확인해주는 함수, 일치하는 코드가 없다면 경고창 띄움, 일치하는 코드가 있다면 <<일치하는 코드의 페이지>>로 연결
+'''
+def codeconfirm(request, rollings_id):
+    my_roll = RollingInfo.objects.get(pk=rollings_id)
+    if request.method == 'POST' :
+        rolling_id = request.POST.get('rolling_id') #안되면 get 지우기
+        confirm = authenticate(rolling_id=rolling_id)
+    
+        if confirm is None:
+            messages.error(request, "Code is not exist !", 
+                            extra_tags='alert alert-warning alert-dismissible fade show')
+
+    return render(request, '/detail/'+str(my_roll.pk))'''
+
+'''
+def codeconfirm(request):
+    my_roll = RollingInfo.objects.get(pk=roll_id)
+    if request.method == 'POST' :
+        roll_id = request.POST.get('codeinput') #안되면 get 지우기
+        confirm = authenticate(roll_id=roll_id)
+    
+        if confirm is None:
+            messages.error(request, "Code is not exist !", 
+                            extra_tags='alert alert-warning alert-dismissible fade show')
+
+    return render(request, '/detail/'+str(my_roll.pk))
+'''
+
+#롤링페이퍼 만들어주는 함수
+def creat_rolling(request):
+    form = CreateForm()
+    if request.method == 'POST' :
+        form = CreateForm(request.POST, request.FILES)
+
+
+#랜덤 코드를 생성해주는 함수 
+'''
+def random_code(length=6):
+    """
+    generates random code of given length
+    """
+    code = base64.urlsafe_b64encode(
+    	uuid.uuid4().bytes.encode("base64").rstrip()
+    ).decode()[:length]
+    
+    #rolling_detail = get_object_or_404(RollingInfo, pk=rolling_id)
+    return render(request, 'detail.html', {'code':code})
+'''
+
+#롤링페이퍼 아이디에 맞는 페이지를 보여준다.
+def detail(request, rolling_id):
+    rolling_detail = get_object_or_404(RollingInfo, pk=rolling_id)
+    return render(request, 'detail.html', {'rolling': rolling_detail})
+    
+'''
+#데드라인이 넘으면
+def deadline(request):
+    today = datetime.today().strftime("%Y%m%d")
+    today = int(today)
+    rollings = RollingInfo.objects.all()'''
+    
