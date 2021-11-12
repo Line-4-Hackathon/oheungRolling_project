a8ecc0b9475f5cd684d94e77960e307031443b7a
Ju <jjuhee0913@gmail.com>
2021-11-12 20:39:51 +0900

    Add RellingInfo, Comment model

--- rollingPaper/models.py
+++ rollingPaper/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class RollingInfo(models.Model) :
+	rolling_name = models.CharField(max_length=15)
+	recipient_name = models.CharField(max_length=5)
+    # present_date = models.DateField(null = True)
+	code = models.CharField(max_length=6) # 최대 6자리의 코드 저장
+    
+class Comment(models.Model) :
+	writer = models.CharField(max_length=5)
+	description = models.TextField()
+	password = models.IntegerField()
