e2945ef951192e2e8b3efa4d3380c301616fac41
MunDayeon <mndnyn@naver.com>
2021-11-12 23:44:38 +0900

    first_mainpage

--- oheungRolling_project/urls.py
+++ oheungRolling_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import rollingPaper.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',rollingPaper.views.home, name="home"),
 ]
